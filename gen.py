272dfbe3131b111ebff20821846cd5500bd69815
napatswift <napat.do@ku.th>
2023-02-26 00:51:20 +0700

    save in mmocr format

--- gen.py
+++ gen.py
@@ -13,13 +13,16 @@ import cv2
 
 aug_pipeline = ag.AugmentationSequence([
     ag.LowInkRandomLines(),
-    ag.PencilScribbles(size_range=(10, 50), stroke_count_range=(1, 3), count_range=(1, 3), thickness_range=(1, 2), p=.5),
-    ag.Gamma(gamma_range=(.1,.3)),
+    ag.PencilScribbles(size_range=(10, 50), stroke_count_range=(
+        1, 3), count_range=(1, 3), thickness_range=(1, 2), p=.5),
+    ag.Gamma(gamma_range=(.1, .3)),
     ag.LowInkPeriodicLines(),
 ])
 
+
 def arabic2th(n):
     return chr(ord(n)+(ord('๑')-ord('1')))
+
 
 class DocTemplate:
     def __init__(self,):
@@ -34,9 +37,11 @@ class DocTemplate:
         self.next = (self.next + 1) % len(self.templates)
         return template
 
+
 politician = pd.read_csv('data/TWFU-PoliticianData.csv', skiprows=1)
 doc_template_gen = DocTemplate()
-faker=Faker('th')
+faker = Faker('th')
+
 
 def get_token_text(token: str):
     assert token[0] == '{' and token[-1] == '}'
@@ -71,17 +76,20 @@ def get_token_text(token: str):
     if token == 'month':
         return faker.month_name()
     return 'dummy'
-    
+
+
 def get_doc_md(template):
     """return markdown text"""
     tokens = re.findall('\{[^\}]*\}', template)
     for token in tokens:
-        template=template.replace(token, get_token_text(token), 1)
+        template = template.replace(token, get_token_text(token), 1)
     return template
+
 
 def get_font():
     fpaths = random.choice(['fonts/THSarabun.ttf', 'fonts/THSarabun Bold.ttf'])
     return ImageFont.truetype(fpaths, size=random.randrange(26, 28))
+
 
 def create_paper(
     width=1156,
@@ -98,10 +106,10 @@ def put_text(canvas, x, y, text, font):
     for i in range(len(words)):
         word = words[i]
         canvas.text((curr_x, y), word, fill='black', font=font)
-        
-        x0,y0,x1,y1 = canvas.textbbox((curr_x, y), word, font=font)
+
+        x0, y0, x1, y1 = canvas.textbbox((curr_x, y), word, font=font)
         word_bbox.append(dict(polygon=[x0, y0, x1, y0, x1, y1, x0, y1],
-                              bbox=[x0,y0,x1,y1],
+                              bbox=[x0, y0, x1, y1],
                               text=word))
         curr_x = x1
         if i+1 < len(words):
@@ -111,21 +119,23 @@ def put_text(canvas, x, y, text, font):
 
 
 def generate():
-    markdown_parser = mistune.create_markdown(plugins=['table'], renderer='ast')
+    markdown_parser = mistune.create_markdown(
+        plugins=['table'], renderer='ast')
     paper = create_paper()
     canvas = ImageDraw.Draw(paper)
     font = get_font()
     parsed_components = markdown_parser(get_doc_md(doc_template_gen.gen()))
     line_height = font.size + 6
-    position_start={'x': 120, 'y': 150}
-    curr=position_start.copy()
-    
+    position_start = {'x': 120, 'y': 150}
+    curr = position_start.copy()
+
     text_bbox = []
     for component in parsed_components:
         if component['type'] == 'paragraph':
             for c_comp in component['children']:
                 #canvas.text(list(curr.values()), c_comp['text'], fill='black', font=font)
-                args = {**curr, 'text': c_comp['text'], 'font': font, 'canvas': canvas}
+                args = {**curr, 'text': c_comp['text'],
+                        'font': font, 'canvas': canvas}
                 text_bbox.extend(put_text(**args))
                 curr['y'] += line_height
 
@@ -133,7 +143,7 @@ def generate():
             # get number of column
             col_num = None
             col_ratio = []
-            ## get config
+            # get config
             for c_comp in component['children']:
                 if c_comp['type'] == 'table_head':
                     # count number of columns
@@ -145,28 +155,30 @@ def generate():
 
             table_start = position_start['x']
             table_size = paper.size[0] - table_start*2
-            
+
             column_widths = [cr*table_size for cr in col_ratio]
-            
+
             for c_comp in component['children']:
                 for row in c_comp['children']:
                     # skip header
                     if c_comp['type'] == 'table_head':
                         continue
-                    
+
                     for i, cell in enumerate(row['children']):
-                        if len(cell['children']) == 0: continue
-                            
-                        ## position
+                        if len(cell['children']) == 0:
+                            continue
+
+                        # position
                         cell_x_start = sum(column_widths[:i])
-                        if i != 0: cell_x_start += 5
+                        if i != 0:
+                            cell_x_start += 5
                         cell_width = column_widths[i]
                         x = table_start+cell_x_start
                         ####################
-                        
+
                         text = cell['children'][0]['text']
-                        
-                        ## alignment of text
+
+                        # alignment of text
                         if cell['align'] == 'right':
                             text_w = font.getlength(text)
                             x = x + cell_width - text_w
@@ -174,14 +186,14 @@ def generate():
                             text_w = font.getlength(text)
                             x = x + cell_width/2 - text_w/2
                         ####################
-                        
+
                         args = {
-                                'x': x,
-                                'y': curr['y'],
-                                'text': text,
-                                'font': font,
-                                'canvas': canvas
-                               }
+                            'x': x,
+                            'y': curr['y'],
+                            'text': text,
+                            'font': font,
+                            'canvas': canvas
+                        }
                         text_bbox.extend(put_text(**args))
 
                     curr['y'] += line_height
@@ -194,8 +206,11 @@ def generate():
                     curr['y'] += line_height
         else:
             print(component)
-
+    for tbox in text_bbox:
+        tbox['bbox_label'] = 0
+        tbox['ignore'] = False
     return paper, text_bbox
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -204,11 +219,11 @@ if __name__ == '__main__':
     parser.add_argument('--output_dir', default='output')
 
     args = parser.parse_args()
-    
+
     img_dir = 'imgs'
 
     os.makedirs(os.path.join(args.output_dir, img_dir), exist_ok=True)
-    
+
     img_counter = 0
 
     image_bbox_list = []
@@ -221,7 +236,7 @@ if __name__ == '__main__':
             width=img.size[0],
             height=img.size[1]
         )
-        
+
         for j in range(args.copy):
             img_path = os.path.join(img_dir, f'img_{img_counter}.jpg')
             aug_img = aug_pipeline(np.array(img))[0]
@@ -233,5 +248,15 @@ if __name__ == '__main__':
             image_bbox_list.append(m)
             img_counter += 1
 
-    with open(os.path.join(args.output_dir, 'gt.json',), 'w') as fp:
-        json.dump(image_bbox_list, fp, ensure_ascii=False,)
+    train_size = int(len(image_bbox_list) * .8)
+    metainfo = dict(dataset_type='TextDetDataset',
+                    task_name='textdet', category=dict(id=0, name='text'))
+
+    with open(os.path.join(args.output_dir, 'textdet_train.json',), 'w') as fp:
+
+        json.dump(dict(metainfo=metainfo, data_list=image_bbox_list[:train_size]),
+                  fp, ensure_ascii=False,)
+
+    with open(os.path.join(args.output_dir, 'textdet_test.json',), 'w') as fp:
+        json.dump(dict(metainfo=metainfo, data_list=image_bbox_list[train_size:]),
+                  fp, ensure_ascii=False,)
